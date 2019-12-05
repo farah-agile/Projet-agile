aa591ba960dde23856738aebced24675e3f46662
terabasic <yeelee.vip@gmail.com>
2019-12-05 21:13:56 +0100

    cucumber

--- TP3_Agile/src/test/java/cours/SalaryProfDefinitions.java
+++ TP3_Agile/src/test/java/cours/SalaryProfDefinitions.java
@@ -23,7 +23,7 @@ public class SalaryProfDefinitions {
 		 // Write code here that turns the phrase above into concrete actions
      prof1 = new Professeur(25, "Benoit");
      coursM = new CoursMagistral("math",200);
-     coursTD = new TravauxDirigeDecorator(coursTD);
+     coursTD = new TravauxDirigeDecorator(coursM);
     
      prof1.addCours(coursM);
      prof1.addCours(coursTD);
@@ -32,11 +32,11 @@ public class SalaryProfDefinitions {
   @When("Affectation de nouveau_cours au professeur")
   public void Affectation__de_nouveau_cours_au_professeur() {
 	    // Write code here that turns the phrase above into concrete actions
-		 coursTP = new TravauxPratiqueDecorator(coursTP);
+		 coursTP = new TravauxPratiqueDecorator(coursM);
 		 prof1.addCours(coursTP);
 	}
 
-  @Then(" La salaire de professeur est calcule")
+  @Then("La salaire de professeur est calcule")
   public void Le_salaire_de_professeur_est_calcule() {
 	    // Write code here that turns the phrase above into concrete actions
 		double total =prof1.calculSalary();
