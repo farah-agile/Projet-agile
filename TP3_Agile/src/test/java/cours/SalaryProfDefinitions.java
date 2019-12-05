4723b698a65196d75370409687cfa8f8ea96be24
Tinayii00 <wangyutianyi@gmail.com>
2019-12-06 03:54:50 +0800

    salary test change 2

---
+++ TP3_Agile/src/test/java/cours/SalaryProfDefinitions.java
@@ -0,0 +1,20 @@
+package cours;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+import cucumber.api.java.en.Then;
+
+public class SalaryProfDefinitions {
+  @Given("^you are in Given annotation$")
+  public void given() throws Throwable {
+  }
+
+  @When("^you are in When annotation$")
+  public void when() throws Throwable {
+  }
+
+  @Then("^you are in Then annotation$")
+  public void then() throws Throwable {
+  }
+
+}
